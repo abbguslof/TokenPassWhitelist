372e0f8c148f7296586ccc9fd19d17250d2b896c
Gustav Löfqvist <gustav@lofqvist.ws>
2025-05-28 18:44:50 +0200

    updated build

--- tokenpass-frontend/svelte.config.js
+++ tokenpass-frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -17,9 +17,11 @@ const config = {
 
 export default {
 	kit: {
-	  adapter: adapter(),
-	  paths: {
-		base: '',
+	  adapter: adapter({
+		out: 'build'
+	  }),
+	  alias: {
+		$lib: 'src/lib'
 	  }
 	}
   };
